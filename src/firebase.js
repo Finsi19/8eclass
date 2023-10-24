33e938fdef9b213985110cb479d8b18623004e1a
Finsi19 <zfino52@gmail.com>
2023-10-24 01:33:37 -0700

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -10,12 +10,13 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBmUIXJblRGF4HTCO3hOB8MykvLOq7whUE",
-  authDomain: "project-web-kelas.firebaseapp.com",
-  projectId: "project-web-kelas",
-  storageBucket: "project-web-kelas.appspot.com",
-  messagingSenderId: "202691312352",
-  appId: "1:202691312352:web:e82e7a494e705b2a12ee85"
+  apiKey: "AIzaSyDopD_nJx8qqf0vnrlMBl2pbToqb9V3xOo",
+  authDomain: "esmpn19.firebaseapp.com",
+  projectId: "esmpn19",
+  storageBucket: "esmpn19.appspot.com",
+  messagingSenderId: "10145197571",
+  appId: "1:10145197571:web:44eb9093d0105dcae7e836",
+  measurementId: "G-33RDVGMCPN"
 };
 
 // Initialize Firebase
